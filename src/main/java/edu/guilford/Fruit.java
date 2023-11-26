57c86b94f95f050331ed4f594ba7a1d511ecb0eb
hzhang00 <zhangkaren1017@gmail.com>
2023-11-26 10:36:20 -0500

    added some basic stuff (defined classes)

---
+++ src/main/java/edu/guilford/Fruit.java
@@ -0,0 +1,19 @@
+package edu.guilford;
+
+public class Fruit {
+    public static void main(String[] args) {
+
+    }
+    //to define the proportions of the fruits using x, y, and the radius (becuase they are all circles)
+        int x, y, radius;
+    
+    //constructors to initialize each fruit
+        public Fruit(int x, int y, int radius) {
+            this.x = x;
+            this.y = y;
+            this.radius = radius;
+        }
+
+    //add more stuff for the behaviors of the fruits -- i think falling, the scoreboard, etc
+
+}
--- src/main/java/edu/guilford/jpanel.java
+++ src/main/java/edu/guilford/jpanel.java
@@ -1,5 +1,12 @@
 package edu.guilford;
 
+import javax.swing.JPanel;
+
 public class jpanel {
+    private JPanel fruits;
     
+    class fruitsPanel extends JPanel {
+   
+    }
+
 }
