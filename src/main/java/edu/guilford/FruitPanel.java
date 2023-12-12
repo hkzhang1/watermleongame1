9ace9c87808e61306bf8f83696dd13a4e6f30dc1
hzhang00 <zhangkaren1017@gmail.com>
2023-12-11 19:05:39 -0500

    started working on compiling everything onto fruitPanel
    Co-authored-by: afuad727 <afuad727@users.noreply.github.com>

--- src/main/java/edu/guilford/FruitPanel.java
+++ src/main/java/edu/guilford/FruitPanel.java
@@ -14,6 +14,13 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
+//plan:
+//Fruit Panel = big guy
+//inside it, we instantiate our variables, put our actions in methods (like ball action into methods, mouse key events) and organize the look of the panel
+//so one method is ballaction, one is randomball (already made), and one needs to be randomgenerate location or something
+//one needs to randomly choose a ball from the balls that we have to appear at the top
+//we have to first start with just making one ball appear and then making it fall. if we can figure that out today, then tmw will be big dubs
+
 
 public class FruitPanel extends JPanel{
     //declaring things
@@ -24,62 +31,141 @@ public class FruitPanel extends JPanel{
     private Fruit grape;
     private Fruit orange;
     private Fruit apple;
+    private Fruit bomb;
 
-        private List<Fruit> fruits;
-        
-        public void MouseKeyEventPanel(){
-            //super();
-            initPanel();
-        }
-        
-        
-        public fruitsMovementPanel() { // for other code in this class << we gotta change this name what im confusing it w the fruitmovement in initpanel
-            fruits = new ArrayList<>();
-            //fruits.add(new Fruit(50, 50, 30, "Pineapple", Color.YELLOW, 0)); //change later to specific sizes, more sizes (how many?)
-            //fruits.add(new Fruit(150, 100, 40, "Watermelon", Color.GREEN, 0));
-            // cherry = new Fruit(0, 0, 20, "cherry", Color.PINK, 0);
-            // strawberry = new Fruit(300, 150, 40, "strawberry", Color.MAGENTA, 0);
-            // grape = new Fruit(350, 150, 60, "grape", Color.BLUE, 0);
-            // orange = new Fruit(400, 150, 80, "orange", Color.ORANGE, 0);
-            // apple = new Fruit(450, 150, 100, "apple", Color.RED, 0);
-            
-            //fruitsPanel.add(cherry); //trying to add the fruit objects to the main jpanel
-            // fruitsPanel.add(strawberry);
-            // fruitsPanel.add(grape);
-            // fruitsPanel.add(orange);
-            // fruitsPanel.add(apple);
+    //vector code (but maybe not necessary? goes from 36 to 67)
+    // class Vector { //is this supposed to be in the ball falling part? or should it be separate? this feels like some basic stuff  that should be above or smth not in the loop
+    //     private double x;
+    //     private double y;
+    //     private double z;
 
-            fruits.add(cherry); //trying to add the fruit objects to the main jpanel
-            fruits.add(strawberry);
-            fruits.add(grape);
-            fruits.add(orange);
-            fruits.add(apple);
-            
-            
-            for (Fruit fruit : fruits) { //tells the user what type of fruit it is
-                System.out.println(fruit.getType());
+    //     public Vector(double x, double y, double z) {
+    //         this.x = x;
+    //         this.y = y;
+    //         this.z = z;
+    //     }
+
+    //     public double getX() {
+    //         return x;
+    //     }
+
+    //     public double getY() {
+    //         return y;
+    //     }
+
+    //     public double getZ() {
+    //         return z;
+    //     }
+
+    //     public Vector add(Vector other) {
+    //         return new Vector(x + other.x, y + other.y, z + other.z);
+    //     }
+
+    //     public Vector multiply(double scalar) {
+    //         return new Vector(x * scalar, y * scalar, z * scalar);
+    //     }
+    // }
+
+    //mousekeyevent (look at lab 14 on how they formatted it)
+        
+    public void MouseKeyEventPanel(){
+        //super();
+        initPanel();
+    }
+    
+    //ball action event (somehow move it from ballaction.java) --> will be easier to call upon things
+    public void ballFalling() {
+
+        double g = -9.81;
+        //double dropheight = 20;
+        double height = 15;
+        //double width = 15;
+        double t = 0;
+        double dt = 0.01;
+
+        // Sphere cherry = new Sphere(dropheight, 0.5, Color.RED, g);
+        // Sphere strawberry = new Sphere(dropheight, 1, Color.MAGENTA, g);
+        // Sphere grape = new Sphere(dropheight, 1.5, Color.BLUE, g);
+        // Sphere orange = new Sphere(dropheight, 2, Color.ORANGE, g);
+        // Sphere apple = new Sphere(dropheight, 2.5, Color.RED, g);
+        // Sphere bomb = new Sphere(dropheight, 1, Color.BLACK, g);
+
+        // Box bottom = new Box(new Vector(0, -height, 0), new Vector(width, 0, 1), Color.WHITE);
+        // Box left = new Box(new Vector(-width / 2, 0, 0), new Vector(0.1, height, 0.1), Color.WHITE);
+        // Box right = new Box(new Vector(width / 2, 0, 0), new Vector(0.1, height, 0.1), Color.WHITE);
+
+        List<Fruit> list = new ArrayList<>();
+        list.add(cherry);
+        list.add(strawberry);
+        list.add(grape);
+        list.add(orange);
+        list.add(apple);
+        list.add(bomb);
+
+        for (Fruit fruit : list) {
+            while (fruit.getY() - fruit.getRadius() > -height) {
+                double droprate = fruit.getY() + (g * dt);
+                fruit.setY((int)droprate);
+                t += dt;
+                if (fruit.getY() + fruit.radius == bomb.getY() + bomb.radius) {
+                    fruit.color = Color.DARK_GRAY;
+                    System.out.println("exploded!");
+                }
             }
-
-            setSize(400, 400);
-            setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            add(new fruitsPanel(fruits));
-        }
-        public void initPanel(){
-            setPreferredSize(new Dimension(1800,900));
-            cherry = new Fruit(0, 0, 20, "cherry", Color.PINK, 0);
-            strawberry = new Fruit(300, 150, 40, "strawberry", Color.MAGENTA, 0);
-            grape = new Fruit(350, 150, 60, "grape", Color.BLUE, 0);
-            orange = new Fruit(400, 150, 80, "orange", Color.ORANGE, 0);
-            apple = new Fruit(450, 150, 100, "apple", Color.RED, 0);
-            bomb = new Fruit (500, 120, 20, "bomb", Color.GRAY, 0);
-
-            fruitMovement = new JPanel();
-            fruitMovementPanel.setOpaque(true);
-            fruitMovementPanel.setLayout(new BoxLayout(fruitMovementPanel, BoxLayout.LINE_AXIS));            
-            fruitMovementPanel.add(Box.createHorizontalGlue());
-            fruitMovement. 
         }
     }
+
+    
+    
+    // for other code in this class
+    private void attributesPanel() {//whats the return type right here no clue what does it do again) {  << we gotta change this name what im confusing it w the fruitmovement in initpanel
+        fruits = new ArrayList<>();
+        //fruits.add(new Fruit(50, 50, 30, "Pineapple", Color.YELLOW, 0)); //change later to specific sizes, more sizes (how many?)
+        //fruits.add(new Fruit(150, 100, 40, "Watermelon", Color.GREEN, 0));
+        // cherry = new Fruit(0, 0, 20, "cherry", Color.PINK, 0);
+        // strawberry = new Fruit(300, 150, 40, "strawberry", Color.MAGENTA, 0);
+        // grape = new Fruit(350, 150, 60, "grape", Color.BLUE, 0);
+        // orange = new Fruit(400, 150, 80, "orange", Color.ORANGE, 0);
+        // apple = new Fruit(450, 150, 100, "apple", Color.RED, 0);
+        
+        //fruitsPanel.add(cherry); //trying to add the fruit objects to the main jpanel
+        // fruitsPanel.add(strawberry);
+        // fruitsPanel.add(grape);
+        // fruitsPanel.add(orange);
+        // fruitsPanel.add(apple);
+
+        fruits.add(cherry); //trying to add the fruit objects to the main jpanel
+        fruits.add(strawberry);
+        fruits.add(grape);
+        fruits.add(orange);
+        fruits.add(apple);
+        
+        
+        for (Fruit fruit : fruits) { //tells the user what type of fruit it is
+            System.out.println(fruit.getType());
+        }
+
+        setSize(400, 400);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        add(new fruitPanel(fruits)); //bruh what does this even mean 
+        //the code does nothing like i:A????? ok wtv can i delete or change
+    }
+    public void initPanel(){
+        setPreferredSize(new Dimension(1800,900));
+        cherry = new Fruit(0, 0, 20, "cherry", Color.PINK, 0);
+        strawberry = new Fruit(300, 150, 40, "strawberry", Color.MAGENTA, 0);
+        grape = new Fruit(350, 150, 60, "grape", Color.BLUE, 0);
+        orange = new Fruit(400, 150, 80, "orange", Color.ORANGE, 0);
+        apple = new Fruit(450, 150, 100, "apple", Color.RED, 0);
+        bomb = new Fruit (500, 120, 20, "bomb", Color.GRAY, 0);
+
+        fruitMovementPanel = new JPanel();
+        fruitMovementPanel.setOpaque(true);
+        fruitMovementPanel.setLayout(new BoxLayout(fruitMovementPanel, BoxLayout.LINE_AXIS));            
+        fruitMovementPanel.add(Box.createHorizontalGlue());
+        //fruitMovement. 
+    }
+
 
 
     public void generateRandomFruit() { //generates a random fruit
@@ -91,7 +177,7 @@ public class FruitPanel extends JPanel{
         Color randomballColor = Color.CYAN;
         double randomVelocity = (random.nextDouble() * 10);
         Fruit randomFruit = new Fruit(randomX, randomY, randomRadius, randomType, randomballColor, randomVelocity);
-    
+
     public class fruitsPanel extends JPanel {
         private List<Fruit> fruits;
 
@@ -138,4 +224,5 @@ public class FruitPanel extends JPanel{
 
         // Repaint the panel to update the fruit positions
         //fruitsPanel.repaint();
-    }
+}
+}
